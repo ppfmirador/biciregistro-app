ef92ae28a4fec9968c2ccd81afc79e68ebbbdeba
Erika López <holamirador@pizzaperfavore.com>
2025-07-16 22:44:15 +0000

    Edita next.config.js (raíz del proyecto) y agrega:
    /** @type {import('n

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,30 @@
 
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
-      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
     ],
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination:
+          'https://bike-guardian-hbbg6.firebaseapp.com/__/auth/:path*',
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
