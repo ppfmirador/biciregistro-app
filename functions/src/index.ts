728c6000f565c27a91c38b9d0cdb998c21e1e3a7
Erika López <holamirador@pizzaperfavore.com>
2025-07-21 21:57:18 +0000

    Este error sucede al intentar el despliegue, corrigelo
    =============
    Ru

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+
 // functions/src/index.ts
 import { onCall, HttpsError } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
@@ -9,7 +10,7 @@ import type {
   UserRole,
   CorsConfigItem,
 } from "./types";
-import corsConfig from "../../cors.json";
+import corsConfig from "../cors.json";
 
 // Initialize Firebase Admin SDK
 admin.initializeApp();
